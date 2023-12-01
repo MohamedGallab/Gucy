91466dac79433bafb4b1c56a05f595cec043bf6a
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2023-12-01 22:46:12 +0200

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:gucy/main_widgets/main_scaffold.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -12,15 +14,24 @@ Future<void> main() async {
   runApp(const MainApp());
 }
 
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => MainScaffold(),
+    ),
+  ],
+);
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(useMaterial3: true),
       darkTheme: ThemeData.dark(useMaterial3: true),
-      home: MainScaffold(),
+      routerConfig: _router,
     );
   }
 }
