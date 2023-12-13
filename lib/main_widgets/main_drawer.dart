cb98adb6552ce8c63673351dab858fddbf7fb2a2
IrrationalInteger <amrmohamedyonis@gmail.com>
2023-12-13 16:29:58 +0200

    Finished Sprint 1

--- lib/main_widgets/main_drawer.dart
+++ lib/main_widgets/main_drawer.dart
@@ -1,6 +1,14 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:gucy/providers/user_provider.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class MainDrawer extends StatefulWidget {
   const MainDrawer({super.key});
@@ -10,11 +18,105 @@ class MainDrawer extends StatefulWidget {
 }
 
 class _MainDrawerState extends State<MainDrawer> {
+  final ImagePicker picker = ImagePicker(); // ImagePicker instance
+  final storage = FirebaseStorage.instance;
+
   @override
   Widget build(BuildContext context) {
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final userProvider = Provider.of<UserProvider>(
+      context,
+    );
+
+    Future<void> uploadImage(XFile imageFile) async {
+      try {
+        try {
+          // Get a reference to the file in Firebase Storage from the download URL
+          Reference storageReference =
+              FirebaseStorage.instance.refFromURL(userProvider.user["picture"]);
+          await storageReference.getDownloadURL();
+          await storageReference.delete();
+          print('File deleted from Firebase Storage. URL: ');
+        } catch (e) {}
+        String fileName = basename(imageFile.path);
+
+        Reference storageReference =
+            storage.ref().child('profile_images/$fileName');
+
+        await storageReference.putFile(File(imageFile.path));
+        final String imageUrl = await storageReference.getDownloadURL();
+        userProvider.user["picture"] = imageUrl;
+        userProvider.updateUser(userProvider.user);
+
+        print('Image uploaded to Firebase Storage. URL: $imageUrl');
+      } catch (error) {
+        print('Error uploading image to Firebase Storage: $error');
+      }
+    }
+
+    Future<void> deleteImage() async {
+      try {
+        // Get a reference to the file in Firebase Storage from the download URL
+        Reference storageReference =
+            FirebaseStorage.instance.refFromURL(userProvider.user["picture"]);
+        await storageReference.getDownloadURL();
+        await storageReference.delete();
+        userProvider.user["picture"] = "";
+        userProvider.updateUser(userProvider.user);
+        print('File deleted from Firebase Storage. URL: ');
+      } catch (e) {}
+    }
+
     final TextEditingController controller =
-        TextEditingController(text: (userProvider.email));
+        TextEditingController(text: userProvider.email);
+    void showImageOptions() {
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Wrap(
+            children: [
+              ListTile(
+                leading: const Icon(Icons.camera),
+                title: const Text('Take a photo'),
+                onTap: () async {
+                  // Logic for taking a photo
+                  final pickedFile = await picker.pickImage(
+                    source: ImageSource.camera,
+                  );
+                  if (pickedFile != null) {
+                    await uploadImage(pickedFile);
+                    print('Photo taken: ${pickedFile.path}');
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.image),
+                title: const Text('Choose from gallery'),
+                onTap: () async {
+                  final pickedFile = await picker.pickImage(
+                    source: ImageSource.gallery,
+                  );
+                  if (pickedFile != null) {
+                    await uploadImage(pickedFile);
+                    print('Photo selected: ${pickedFile.path}');
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.remove_circle),
+                title: const Text('Remove photo'),
+                onTap: () async {
+                  await deleteImage();
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -30,12 +132,47 @@ class _MainDrawerState extends State<MainDrawer> {
           ),
         ),
         const SizedBox(height: 20),
-        Image.asset(
-            userProvider.user["picutre"] == ""
-                ? 'assets/default_profile.png'
-                : "",
-            width: 220,
-            height: 220),
+        GestureDetector(
+          onTap: showImageOptions,
+          child: Stack(
+            alignment: Alignment.bottomRight,
+            children: [
+              userProvider.user["picture"] == ""
+                  ? Image.asset(
+                      'assets/default_profile.png',
+                      width: 220,
+                      height: 220,
+                    )
+                  : ClipOval(
+                      child: Container(
+                        width: 220, // Set width as needed
+                        height: 220,
+                        child: Image.network(
+                          // Set height as needed
+                          userProvider.user["picture"],
+                        ),
+                      ),
+                    ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Material(
+                  color: Colors.white,
+                  shape: const CircleBorder(),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SvgPicture.asset(
+                      'assets/file_upload.svg',
+                      width: 24,
+                      height: 24,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
         const SizedBox(height: 20),
         Text(
           (userProvider.user)['name'],
